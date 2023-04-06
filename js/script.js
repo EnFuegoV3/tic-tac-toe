61ccb1005d4d29656f1474075a7527ef3358a390
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-04-05 22:29:03 -1000

    conditional for turns not working, scope? opening new branch to try something else

--- js/script.js
+++ js/script.js
@@ -24,18 +24,36 @@ const human = document.querySelector('.player')
 
 const myGameBoard = (function() {
     const container = document.querySelector('.game-board');
-    
 
-    const gameBoard = ["", "", "", "", "", "", "", "", ""];
-    
-    const xAmount = () => gameBoard.indexOf("X");
-    const oAmount = () => gameBoard.indexOf("O");
+    // const gameBoard = ["", "", "", "", "", "", "", "", ""];
+    const gameBoard = new Array(9).fill(null);
+
+    // let x = () => {
+    //       return gameBoard.filter(x => x === "X").length
+    // }
+
+    // let o = () => {
+    //      return gameBoard.filter(x => x === "O").length;
+    // }
+
+    // const create = () => {
+    //     for(let i=0; i < gameBoard.length; i++) {
+    //         let div = document.createElement('div');
+    //         div.setAttribute('id', [i])
+    //         div.classList.add('squares')
+    //         // div.innerHTML = gameBoard[i];
+    //         container.appendChild(div);
+    //     }
+        
+    //     const squares = document.querySelectorAll('.squares'); 
+    //     return {squares}
+    // }
     
     for(let i=0; i < gameBoard.length; i++) {
         let div = document.createElement('div');
         div.setAttribute('id', [i])
         div.classList.add('squares')
-        div.innerHTML = gameBoard[i];
+        // div.innerHTML = gameBoard[i];
         container.appendChild(div);
     }
         
@@ -43,16 +61,26 @@ const myGameBoard = (function() {
 
     const player1 = () => {squares.forEach(square => {
         square.addEventListener('click', (e) => {
-            gameBoard.splice(e.target.id, 1, player1.symbol)
-            console.log(gameBoard);
+            gameBoard.splice(e.target.id, 1, "X")
+            e.target.innerHTML = "X";
+            
+            console.log(x)
+            console.log()
+            console.log(gameBoard)
+            
             })
         });
     };    
 
     const player2 = () => {squares.forEach(square => {
         square.addEventListener('click', (e) => {
-            gameBoard.splice(e.target.id, 1, player2.symbol)
+            gameBoard.splice(e.target.id, 1, "O")
+            e.target.innerHTML = "O";
+
+            console.log(xx, oo)
+            console.log()
             console.log(gameBoard);
+            
             })
         });
     };  
@@ -62,35 +90,55 @@ const myGameBoard = (function() {
         container,
         player1,
         player2,
-        xAmount,
-        oAmount
+        // x,
+        // o,
+       
 
     }
 
    
 })();
 
+// console.log(x);
 
+// myGameBoard.gameBoard;
 
-// myGameBoard.mark(0, player1.symbol);
+// let x = () => {
+//     return myGameBoard.gameBoard.filter(x => x === "X").length
+// }
 
+// let o = () => {
+//    return myGameBoard.gameBoard.filter(x => x === "O").length;
+// }
 
+let x = myGameBoard.gameBoard.filter(x => x === "X").length
 
+let o = myGameBoard.gameBoard.filter(x => x === "O").length;
 
 
 const displayController = (function () {
-    if(myGameBoard.xAmount() === 0 && myGameBoard.oAmount() === 0){
-         myGameBoard.player1();
-    } else {myGameBoard.player2()}
-    // myGameBoard.player1();
-    // myGameBoard.player2();
+   
+        if(x == 0 && o == 0){
+            myGameBoard.player1();
+            
+            } else if (x && o == 0) {
+                myGameBoard.player2();
+                } else if (x === 1 && o === 1) {
+                    myGameBoard.player1();
+        }
     
 })();
 
+// myGameBoard.board();
+// myGameBoard.x();
+// myGameBoard.o();
+// displayController.game();
+
+// console.log(displayController.player1)
 
 
 
-
+// console.log(myGameBoard.oAmount(), myGameBoard.oAmount())
 
 
 
