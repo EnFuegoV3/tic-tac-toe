f352a4f09486c60a8d41054cfa606e212ce9a579
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-04-07 20:12:40 -1000

    working player v player

--- js/script.js
+++ js/script.js
@@ -9,138 +9,102 @@ const player = (name, symbol) => {
     }
 }
 
-const human = document.querySelector('.player')
-
-// human.addEventListener('click', () => {
-    const player1 = player('player1', "X");
-    const player2 = player('player2', "O");
-//     console.log(player1.symbol);
-// });
-//created two players
-
-
+const player1 = player('player1', "X");
+const player2 = player('player2', "O");
 
 
 
 const myGameBoard = (function() {
     const container = document.querySelector('.game-board');
+    const winner = document.querySelector('.player-board')
+    
 
-    // const gameBoard = ["", "", "", "", "", "", "", "", ""];
     const gameBoard = new Array(9).fill(null);
 
-    // let x = () => {
-    //       return gameBoard.filter(x => x === "X").length
-    // }
-
-    // let o = () => {
-    //      return gameBoard.filter(x => x === "O").length;
-    // }
-
-    // const create = () => {
-    //     for(let i=0; i < gameBoard.length; i++) {
-    //         let div = document.createElement('div');
-    //         div.setAttribute('id', [i])
-    //         div.classList.add('squares')
-    //         // div.innerHTML = gameBoard[i];
-    //         container.appendChild(div);
-    //     }
-        
-    //     const squares = document.querySelectorAll('.squares'); 
-    //     return {squares}
-    // }
-    
     for(let i=0; i < gameBoard.length; i++) {
         let div = document.createElement('div');
         div.setAttribute('id', [i])
         div.classList.add('squares')
-        // div.innerHTML = gameBoard[i];
         container.appendChild(div);
     }
+
+    function winning(y) {
+        if((gameBoard[0] == y && gameBoard[1] == y && gameBoard[2] == y) ||
+        (gameBoard[3] == y && gameBoard[4] == y && gameBoard[5] == y) ||
+        (gameBoard[6] == y && gameBoard[7] == y && gameBoard[8] == y) ||
+        (gameBoard[0] == y && gameBoard[3] == y && gameBoard[6] == y) ||
+        (gameBoard[1] == y && gameBoard[4] == y && gameBoard[7] == y) ||
+        (gameBoard[2] == y && gameBoard[5] == y && gameBoard[8] == y) ||
+        (gameBoard[0] == y && gameBoard[4] == y && gameBoard[8] == y) ||
+        (gameBoard[2] == y && gameBoard[4] == y && gameBoard[6] == y))
         
-    const squares = document.querySelectorAll('.squares');
-
-    const player1 = () => {squares.forEach(square => {
-        square.addEventListener('click', (e) => {
-            gameBoard.splice(e.target.id, 1, "X")
-            e.target.innerHTML = "X";
-            
-            console.log(x)
-            console.log()
-            console.log(gameBoard)
-            
-            })
-        });
-    };    
-
-    const player2 = () => {squares.forEach(square => {
-        square.addEventListener('click', (e) => {
-            gameBoard.splice(e.target.id, 1, "O")
-            e.target.innerHTML = "O";
-
-            console.log(xx, oo)
-            console.log()
-            console.log(gameBoard);
-            
-            })
-        });
-    };  
-
-    return {
-        gameBoard,
-        container,
-        player1,
-        player2,
-        // x,
-        // o,
-       
+        return "win"
 
     }
 
+    const win = () => {
+        if(winning("X") == "win") {
+            winner.textContent = "Player 1 Wins"
+        } else if(winning("O") == "win") {
+            winner.textContent = "Player 2 Wins"
+        }
+        
+    }
+
+    return {
+       gameBoard,
+       win,
+       winning
+    }
    
 })();
-
-// console.log(x);
-
-// myGameBoard.gameBoard;
-
-// let x = () => {
-//     return myGameBoard.gameBoard.filter(x => x === "X").length
-// }
-
-// let o = () => {
-//    return myGameBoard.gameBoard.filter(x => x === "O").length;
-// }
-
-let x = myGameBoard.gameBoard.filter(x => x === "X").length
-
-let o = myGameBoard.gameBoard.filter(x => x === "O").length;
 
 
 const displayController = (function () {
-   
-        if(x == 0 && o == 0){
-            myGameBoard.player1();
-            
-            } else if (x && o == 0) {
-                myGameBoard.player2();
-                } else if (x === 1 && o === 1) {
-                    myGameBoard.player1();
-        }
+    const squares = document.querySelectorAll('.squares');
+    const p1 = document.querySelector('#p1');
+    const p2 = document.querySelector('#p2');
+    const reset = document.querySelector('.reset')
+
+    reset.addEventListener('click', () => {
+        location.reload();
+    })
+
+    const x = () => {
+        return myGameBoard.gameBoard.filter(x => x === "X").length
+    }
     
+    let o = () => {
+        return myGameBoard.gameBoard.filter(o => o === "O").length;
+    }
+
+    if(x() == o()){
+        p1.classList.add('turn')
+    }
+
+    squares.forEach(square => {
+        square.addEventListener('click', (e) => {
+            if(x() == o()) {
+                p1.classList.remove('turn');
+                p2.classList.add('turn');
+                myGameBoard.gameBoard.splice(e.target.id, 1, "X");
+                e.target.innerHTML = "X";
+                myGameBoard.win();
+            } else {
+                p2.classList.remove('turn');
+                p1.classList.add('turn');
+                myGameBoard.gameBoard.splice(e.target.id, 1, "O");
+                e.target.innerHTML = "O";
+                myGameBoard.win();
+                } 
+            
+        }, {once: true})
+    });
+
 })();
 
-// myGameBoard.board();
-// myGameBoard.x();
-// myGameBoard.o();
 // displayController.game();
-
-// console.log(displayController.player1)
-
-
-
-// console.log(myGameBoard.oAmount(), myGameBoard.oAmount())
-
-
-
-// myGameBoard.create();
+// console.log(x() == o());
+// displayController.player1();
+// displayController.player2();
 
